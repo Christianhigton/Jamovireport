7e86a9158157d2fa996a7af09d672d44cee607e4
ChristianHigton <christianhigton@gmail.com>
2026-05-28 19:49:08 +0100

    Add guided MANOVA menu analysis

---
+++ R/eduMancova.b.R
@@ -0,0 +1,32 @@
+#' @importFrom jmvcore .
+eduMancovaClass <- if (requireNamespace("jmvcore", quietly = TRUE)) R6Class(
+    "eduMancovaClass",
+    inherit = eduMancovaBase,
+    private = list(
+        .run = function() {
+            outcomes <- self$options$outcomes
+            factors <- self$options$factors
+            covariates <- self$options$covariates
+            if (is.null(factors))
+                factors <- character()
+            if (is.null(covariates))
+                covariates <- character()
+            if (length(outcomes) < 2L || length(c(factors, covariates)) == 0L)
+                return()
+            result <- edu_manova(self$data, outcomes, factors, covariates)
+            result <- .jr_apply_variable_descriptions(result, self$data)
+            self$results$overview$setContent(.jr_jamovi_overview_html(result))
+            for (i in seq_len(nrow(result$statistics)))
+                self$results$main$addRow(rowKey = i, values = as.list(result$statistics[i, ]))
+            for (i in seq_len(nrow(result$descriptives))) {
+                d <- result$descriptives[i, ]
+                self$results$descriptives$addRow(rowKey = i, values = list(
+                    label = d$group, n = d$n, mean = d$mean, sd = d$sd
+                ))
+            }
+            .jr_populate_diagnostics(self$results$diagnostics, result$diagnostics)
+            self$results$report$setContent(.jr_jamovi_report_html(result, self$options))
+            self$results$interpretation$setContent(.jr_jamovi_interpretation_html(result))
+        }
+    )
+)
